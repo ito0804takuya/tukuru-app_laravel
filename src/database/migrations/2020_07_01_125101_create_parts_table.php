7703b0d39576621a7539a490d8bde433fa5fb3a8
ito0804takuya <ito0804takuya@gmail.com>
2020-07-01 22:09:03 +0900

    partsモデルとテーブル作成

---
+++ src/database/migrations/2020_07_01_125101_create_parts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('parts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->unique();
+            $table->integer('supplier_id');
+            $table->integer('created_user_id');
+            $table->integer('updated_user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('parts');
+    }
+}
