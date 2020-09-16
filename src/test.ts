bc6b98eb81a251626a9bedebc05711bbf77d8840
ito0804takuya <ito0804takuya@gmail.com>
2020-09-17 00:47:48 +0900

    test.ts ステータスコード検証追加

--- src/test.ts
+++ src/test.ts
@@ -1,17 +1,22 @@
-import "testcafe";
-import {Selector} from "testcafe";
+import {Selector, RequestLogger} from "testcafe";
 
-fixture("TUKURU Login")
-    .page("http://13.231.189.5/login");
+const url = "http://13.231.189.5/login";
+const emailInputArea = Selector(".form-control").withAttribute("name", "email");
+const passwordInputArea = Selector(".form-control").withAttribute("name", "password");
+const loginButton   = Selector(".loginBtn").withAttribute("name", "commit");
+const logger = RequestLogger(url);
 
-test("TUKURU Login Test", async (t) => {
-    const emailInputArea = Selector(".form-control").withAttribute("name", "email");
-    const passwordInputArea = Selector(".form-control").withAttribute("name", "password");
-    const loginButton   = Selector(".loginBtn").withAttribute("name", "commit");
+fixture("TUKURU")
+    .page(url)
+    .requestHooks(logger);
 
+test
+    .requestHooks(logger)
+    ("Login Test", async (t) => {
     await t
         .typeText(emailInputArea, "tukuru@example.com")
         .typeText(passwordInputArea, "P@ssw0rd")
         .click(loginButton)
+        .expect(logger.requests[0].response.statusCode).eql(200)
         .expect(Selector(".header__userName").innerText).eql('田中 創さん');
 });
